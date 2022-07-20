f2d5be4bd331182829583a33945c789e8b391900
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-20 19:50:48 -0400

    Update and rename streamlit_app.py to proyecto_viz_app.py

---
+++ proyecto_viz_app.py
@@ -0,0 +1,14 @@
+import altair as alt
+import pandas as pd
+import numpy as np
+
+source = pd.DataFrame({
+    'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+})
+
+alt.Chart(source).mark_bar().encode(
+    x='a',
+    y='b'
+)
+
