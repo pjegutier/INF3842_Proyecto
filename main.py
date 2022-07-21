208fd06ac3c75301348489dd74a04f44ce39fe99
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-20 21:43:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,12 @@ import sys
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", 'lxml'])
 
-data_set = {
-    'countries': ['India', 'Australia', 'Japan', 'America', 'Russia'],
-    'values': [4500, 2500, 1053, 500, 3200]
-}
-
-df = pd.DataFrame(data_set)
+bici = pd.read_csv('san-juan-xl.csv',sep=';')
 
 line = alt.Chart(df).mark_line().encode(
     x = 'countries',
     y = 'values'
 ).properties(width=500, height=500)
-
-bici = pd.read_csv('san-juan-xl.csv',sep=';')
 
 st.altair_chart(line)
 st.write(bici.head())
