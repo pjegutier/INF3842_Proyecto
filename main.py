dfca0826dd9d98a281117f06358ae15928104ce8
LuisManuelCamposRamos <109704832+LuisManuelCamposRamos@users.noreply.github.com>
2022-07-20 22:32:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ line = alt.Chart(bici).mark_circle(size=60).encode(
     y = 'ns1:LongitudeDegrees5:Q'
 ).properties(width=500, height=500).interactive()
 
-st.write('Se escoge un dataset de página wikiloc (https://es.wikiloc.com/) debido a que buscamos un tema de visualización que nos motivara.\n
+st.text('Se escoge un dataset de página wikiloc (https://es.wikiloc.com/) debido a que buscamos un tema de visualización que nos motivara.\n
           El dataset tiene atributos como altitud, longitud, latitud, velocidad, distancia, variables que permiten analizar una ruta desde\n
           multiples formas de visualización')
 
