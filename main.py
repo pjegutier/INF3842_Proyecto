a50e7446f03ff3c7b6b7d58d1b121c4bdbe69497
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-20 22:15:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ subprocess.check_call([sys.executable, "-m", "pip", "install", 'lxml'])
 
 bici = pd.read_csv('san-juan-xl.csv',sep=';',decimal=',')
 
-line = alt.Chart(bici).mark_circle(size=60).encode(
+line = alt.Chart(bici).mark_circle(size=100).encode(
     x = 'ns1:LatitudeDegrees4:Q',
     y = 'ns1:LongitudeDegrees5:Q'
 ).properties(width=500, height=500).interactive()
