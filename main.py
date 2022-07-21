14d226d94ea9cd13e5c0a8033512fafcb914f7e6
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-20 22:08:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import sys
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", 'lxml'])
 
-bici = pd.read_csv('san-juan-xl.csv',sep=';')
+bici = pd.read_csv('san-juan-xl.csv',sep=';',decimal=',')
 
-# line = alt.Chart(df).mark_line().encode(
-#     x = 'countries',
-#     y = 'values'
-# ).properties(width=500, height=500)
+line = alt.Chart(bici).mark_line().encode(
+    x = 'ns1:LatitudeDegrees4',
+    y = 'ns1:LongitudeDegrees5'
+).properties(width=500, height=500)
 
-# st.altair_chart(line)
+st.altair_chart(line)
 st.write(bici.head())
 
