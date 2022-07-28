9ebdb4c9c583d0e522288ad35392c6e60b0c7b9f
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-28 00:04:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,6 @@ import subprocess
 import sys
 import pydeck as pdk
 
-# # funcion
-# def load_data():
-#     data = pd.read_csv(
-#         "uber-raw-data-sep14.csv.gz",
-#         nrows=100000,  # approx. 10% of data
-#         names=[
-#             "date/time",
-#             "lat",
-#             "lon",
-#         ],  # specify names directly since they don't change
-#         skiprows=1,  # don't read header since names specified directly
-#         usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
-#         parse_dates=[
-#             "date/time"
-#         ],  # set as datetime instead of converting after the fact
-#     )
-
-#     return data
-
-
 # cargamos base de datos
 san_juan = pd.read_csv('san-juan-xl.csv',sep=';',decimal=',')
 buitrera = pd.read_csv('buitrera-lastorres.csv',sep=';',decimal=',')
@@ -35,8 +15,6 @@ lastorres = pd.read_csv('lastorres-pinoskytrail-xl.csv',sep=';',decimal=',')
 pabellones = pd.read_csv('pabellones.csv',sep=';',decimal=',')
 
 st.set_page_config(layout="wide", page_icon="🚲", page_title="Rutas en Bici")
-
-
 
 st.write('')
 st.write('')
@@ -54,7 +32,6 @@ with row1_1:
     
     """
     )
-
 
 with row1_2:
     st.write(
@@ -86,18 +63,14 @@ with row1_2:
 #     else:
 #         st.warning("You have to select only 2 locations")
 
-        #     rutas = source.symbol.unique()
+#     rutas = source.symbol.unique()
 #     lista_rutas = st.multiselect("Choose stocks to visualize", all_symbols, all_symbols[:3])
-
 
 row2_1, row2_2, row2_3 = st.columns((1, 1, 1))
 
 zoom = 12    
-# lat = np.average(san_juan['ns1:LatitudeDegrees4'])
-# lon = np.average(san_juan['ns1:LongitudeDegrees5'])
-lat = np.average(buitrera['ns1:LatitudeDegrees4'])
-lon = np.average(buitrera['ns1:LongitudeDegrees5'])
-
+lat = np.average(san_juan['ns1:LatitudeDegrees4'])
+lon = np.average(san_juan['ns1:LongitudeDegrees5'])
     
 with row2_1:
     st.write(
@@ -159,39 +132,5 @@ with row2_3:
         st.altair_chart(line)
     except:
         None
-
-# # segunda ruta
-# with row2_4:
-#     line = alt.Chart(endubaik).mark_circle(size=60).encode(
-#         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
-#         y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Longitud'))
-#     ).properties(width=500, height=500)
-    
-#     st.altair_chart(line)
-
-# # segunda ruta
-# with row2_5:
-#     line = alt.Chart(guindal).mark_circle(size=60).encode(
-#         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
-#         y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Longitud'))
-#     ).properties(width=500, height=500)
-    
-#     st.altair_chart(line)
-
-# with row2_6:
-#     line = alt.Chart(lastorres).mark_circle(size=60).encode(
-#         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
-#         y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Longitud'))
-#     ).properties(width=500, height=500)
-    
-#     st.altair_chart(line)
-
-# with row2_7:
-#     line = alt.Chart(pabellones).mark_circle(size=60).encode(
-#         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
-#         y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Longitud'))
-#     ).properties(width=500, height=500)
-    
-#     st.altair_chart(line)
    
 st.write(san_juan.head())
