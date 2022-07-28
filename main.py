d198890eb7fa92b4b67a9f44076f999dcd8ceac8
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-27 21:10:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -95,12 +95,13 @@ with row2_1:
     )
 
 with row2_2:
-    line = alt.Chart(bici).mark_circle(size=60).encode(
+    st.write(alt.Chart(bici).mark_circle(size=60).encode(
         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
         y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud'))
     ).properties(width=500, height=500).interactive()
+            )
 
-    st.write(''
+st.write(''
               ''
               '')
 
