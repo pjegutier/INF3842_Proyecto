ee0431c3cba27e6181f1a7fcd2c6cc0134c12e59
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-27 21:20:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ lon = np.average(bici['ns1:LongitudeDegrees5'])
 with row2_1:
     st.write(
         pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
+            map_style="mapbox://styles/mapbox/satellite-v9",
             initial_view_state={
                 "latitude": lat,
                 "longitude": lon,
