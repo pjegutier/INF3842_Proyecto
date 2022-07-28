f9134d282b1795858d7a99ac5251955c6e05c0c5
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-27 20:13:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="centered", page_icon="🚲", page_title="Rutas en Bic
 
 # Rutas en Bici
 
-st.title("🚴‍♂️ Rutas en Bici")
+st.title("🚴🏾 Rutas en Bici")
 
 # lista de rutas
 # rutas = source.symbol.unique()
