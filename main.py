499613e0afc5d08b9f6fd96fdb4d61e91c8aa1a3
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-27 20:17:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import pandas as pd
 import streamlit as st
 import subprocess
 import sys
+import pydeck as pdk
+
+# funcion
+def load_data():
+    data = pd.read_csv(
+        "uber-raw-data-sep14.csv.gz",
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
+
 
 # cargamos base de datos
 bici = pd.read_csv('san-juan-xl.csv',sep=';',decimal=',')
