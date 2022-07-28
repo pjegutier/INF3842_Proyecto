f2d5848ba20471209cd80d4886abff89e3a8db5c
LuisManuelCamposRamos <109704832+LuisManuelCamposRamos@users.noreply.github.com>
2022-07-27 22:17:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ with row2_1:
 with row2_2:
     line = alt.Chart(san_juan).mark_circle(size=60).encode(
         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
-        y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud'))
+        y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Longitud'))
     ).properties(width=500, height=500)
     
     st.altair_chart(line)
@@ -125,7 +125,7 @@ with row2_2:
 with row2_3:
     line = alt.Chart(san_juan).mark_circle(size=60).encode(
         x = alt.X('ns1:LatitudeDegrees4:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud')),
-        y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Latitud'))
+        y = alt.Y('ns1:LongitudeDegrees5:Q',scale=alt.Scale(zero=False),axis=alt.Axis(title='Longitud'))
     ).properties(width=500, height=500)
     
     st.altair_chart(line)
