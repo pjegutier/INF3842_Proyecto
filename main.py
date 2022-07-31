648f384ae6c2ab91342772503b7944d178e9745c
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-30 22:59:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ selection = alt.selection_multi(fields=['ns1:Name'], bind='legend')
 chart = alt.Chart(datos_sel).mark_area().encode(
     x=alt.X("dist_total:Q", title ='Distancia Recorrido [m]'),
     y=alt.Y("a_r:Q", stack=None, title = 'Altura Relativa [m]'),
-    color= "ns1:Name:N",
+    color= alt.Color("ns1:Name:N", title = 'Rutas'),
     opacity=alt.condition(selection, alt.value(1.0), alt.value(0.3)) 
 ).properties(width=1300, height=200).add_selection(selection)
 
