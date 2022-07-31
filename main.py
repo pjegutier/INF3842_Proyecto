0cae2ab735d35b912fd268068fb67a69a6a96790
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-30 22:30:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,16 +48,17 @@ with row1_2:
     .
     """
     )
-    def val_sel(): # para seleccionar máximo 2 rutas
-        if len(options) > 2:
-            options = st.multiselect('Ha elegido más de 2 rutas, elija nuevamente',
-                                     df['ns1:Name'].unique(), 
-                                     df['ns1:Name'].unique()[:2], on_change = val_sel())
+   
+    
     # lista de rutas
     options = st.multiselect(
         'Elige hasta 2 rutas para compararlas',
         df['ns1:Name'].unique(), 
-        df['ns1:Name'].unique()[:2], on_change = val_sel())
+        df['ns1:Name'].unique()[:2])
+    
+    if len(options) > 2:
+        options = st.multiselect('Ha elegido más de 2 rutas, elija nuevamente',
+                                 df['ns1:Name'].unique())
     
             
 #     button = st.button("Print Locations",disabled=False)
