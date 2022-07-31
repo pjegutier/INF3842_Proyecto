20e345848e01b47cbee1dbacabc765d04c57deaf
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-30 21:45:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -152,11 +152,11 @@ else:
 
 selection = alt.selection_multi(fields=['ns1:Name'], bind='legend')
 
-chart = alt.Chart(datos_sel).mark_area(opacity=0.3).encode(
+chart = alt.Chart(datos_sel).mark_area().encode(
     x="dist_total:Q",
     y=alt.Y("a_r:Q", stack=None),
     color= "ns1:Name:N",
-    opacity=alt.condition(selection, alt.value(1.0), alt.value(0.0)) 
+    opacity=alt.condition(selection, alt.value(1.0), alt.value(0.3)) 
 ).properties(width=1300, height=200).add_selection(selection)
 
 # mostrar gráfico de altair
