6ff9e11af0d2bd61e5defcebf07e49d9aa1ef251
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-30 20:47:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ chart = alt.Chart(df).mark_area(opacity=0.3).encode(
 # mostrar gráfico de altair
 st.altair_chart(chart)
 
-df[locals()[options[0],locals()[options[1]].head()
+#df[locals()[options[0],locals()[options[1]].head()
 
 
         
