1188edc08845727936674e65a59d44f6da0088d7
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-30 22:25:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,11 +48,11 @@ with row1_2:
     .
     """
     )
-#     def val_sel(): # para seleccionar máximo 2 rutas
-#         if len(options) > 2:
-#             options = st.multiselect('Ha elegido más de 2 rutas, elija nuevamente',
-#                                      df['ns1:Name'].unique(), 
-#                                      df['ns1:Name'].unique()[:2], on_change = val_sel()) 
+    def val_sel(): # para seleccionar máximo 2 rutas
+        if len(options) > 2:
+            options = st.multiselect('Ha elegido más de 2 rutas, elija nuevamente',
+                                     df['ns1:Name'].unique(), 
+                                     df['ns1:Name'].unique()[:2], on_change = val_sel()) 
     # lista de rutas
     options = st.multiselect(
         'Elige hasta 2 rutas para compararlas',
