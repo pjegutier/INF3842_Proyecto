d2c455f5b4ff63db2fe629f09ca767550d2595c0
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-30 21:16:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ chart = alt.Chart(df).mark_area().encode(
 st.altair_chart(chart)
 
 pd.concat([df.loc[df['ns1:Name'] == options[0]],
-           df.loc[df['ns1:Name'] == options[1]]], ignore_index = True)
+           df.loc[df['ns1:Name'] == options[1]]], ignore_index = True).head()
         
 # st.write(san_juan.head())
 st.write(   
