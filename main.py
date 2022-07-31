fddde74e0b76cebb1c4ba2a0c67a0022a9f00809
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-31 00:33:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -59,62 +59,67 @@ with row1_2:
         st.write('Ha elegido más de 2 rutas, elija nuevamente, elimine selecciones')
     
 #parametros
-zoom = 12 
-
-if len(options) == 1:
-    lat = np.mean(df.loc[df['ns1:Name'] == options[0]]['lt'])
-    lon = np.mean(df.loc[df['ns1:Name'] == options[0]]['lng'])    
-elif len(options) == 2:
-    lat = (np.mean(df.loc[df['ns1:Name'] == options[0]]['lt']) + np.mean(df.loc[df['ns1:Name'] == options[1]]['lt']))/2
-    lon = (np.mean(df.loc[df['ns1:Name'] == options[0]]['lng']) + np.mean(df.loc[df['ns1:Name'] == options[1]]['lng']))/2
-    
-else:
-    lat = -34.18082 #machali
-    lon = -70.64933 #machali
-    
+zoom = 12     
 
 row2_1, row2_2, row2_3 = st.columns((1, 1, 1))
 
 with row2_1:
     
+    if len(options) == 1:
+        lat = np.mean(df.loc[df['ns1:Name'] == options[0]]['lt'])
+        lon = np.mean(df.loc[df['ns1:Name'] == options[0]]['lng']) 
+        
+    elif len(options) >= 2:
+        lat = (np.mean(df.loc[df['ns1:Name'] == options[0]]['lt']) + np.mean(df.loc[df['ns1:Name'] == options[1]]['lt']))/2
+        lon = (np.mean(df.loc[df['ns1:Name'] == options[0]]['lng']) + np.mean(df.loc[df['ns1:Name'] == options[1]]['lng']))/2
+    
+    else:
+        lat = -34.18082 #machali
+        lon = -70.64933 #machali
+        
     view_state = pdk.ViewState(
-        longitude = -70.64933,
-        latitude = -34.18082,
+        longitude = lat ,
+        latitude = lon,
         zoom = zoom,
-        pitch = 50)
-     
-    layer1 = pdk.Layer(
+        pitch = 50)   
+    
+    if len(options) == 1:
+        layer1 = pdk.Layer(
         'ScatterplotLayer',
-    df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
-    get_position=['lng', 'lt'],
-    auto_highlight=True,
-    get_radius=50,
-    get_fill_color=[132, 157, 204, 140],
-    pickable=True)
-    
-    layer2 = pdk.Layer(
+        df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
+        get_position=['lng', 'lt'],
+        auto_highlight=True,
+        get_radius=50,
+        get_fill_color=[132, 157, 204, 140],
+        pickable=True)
+        
+        r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1], initial_view_state=view_state)
+        
+        
+    elif len(options) >= 2:
+        layer1 = pdk.Layer(
         'ScatterplotLayer',
-    df.loc[df['ns1:Name'] == options[1]][['lng','lt']],
-    get_position=['lng', 'lt'],
-    auto_highlight=True,
-    get_radius=50,
-    get_fill_color=[290, 187, 105, 140],
-    pickable=True)
+        df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
+        get_position=['lng', 'lt'],
+        auto_highlight=True,
+        get_radius=50,
+        get_fill_color=[132, 157, 204, 140],
+        pickable=True)
+        
+        layer2 = pdk.Layer(
+        'ScatterplotLayer',
+        df.loc[df['ns1:Name'] == options[1]][['lng','lt']],
+        get_position=['lng', 'lt'],
+        auto_highlight=True,
+        get_radius=50,
+        get_fill_color=[290, 187, 105, 140],
+        pickable=True)
+        
+        r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1,layer2], initial_view_state=view_state)
     
-#     if len(options) == 1:
-#         lat = np.mean(df.loc[df['ns1:Name'] == options[0]]['lt'])
-#         lon = np.mean(df.loc[df['ns1:Name'] == options[0]]['lng'])    
-#     elif len(options) == 2:
-#         lat = (np.mean(df.loc[df['ns1:Name'] == options[0]]['lt']) + np.mean(df.loc[df['ns1:Name'] == options[1]]['lt']))/2
-#         lon = (np.mean(df.loc[df['ns1:Name'] == options[0]]['lng']) + np.mean(df.loc[df['ns1:Name'] == options[1]]['lng']))/2
-    
-# else:
-#     lat = -34.18082 #machali
-#     lon = -70.64933 #machali
-    
-#     if 
-    
-    r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1,layer2], initial_view_state=view_state)
+    else:
+        r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", initial_view_state=view_state)
+        
     st.write(r)
 
 
