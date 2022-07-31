90855ddfbfd78ebe87b44406eee7994584982bba
pjegutier <109693933+pjegutier@users.noreply.github.com>
2022-07-31 01:04:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -70,43 +70,72 @@ elif len(options) >= 2:
     
 else:
     lat = -34.18082 #machali
-    lon = -70.64933 #machali
-
-# lat = round(lat,5)
-# lon = round(lon,5)
-    
+    lon = -70.64933 #machali    
 
 row2_1, row2_2, row2_3 = st.columns((1, 1, 1))
 
 with row2_1:
-    
-    st.write(lat,lon)
     
     view_state = pdk.ViewState(
         longitude = lon,
         latitude = lat,
         zoom = zoom,
         pitch = 50)
+    
+    if len(options) == 1:
+        layer1 = pdk.Layer(
+            'ScatterplotLayer',
+            df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
+            get_position=['lng', 'lt'],
+            auto_highlight=True,
+            get_radius=50,
+            get_fill_color=[132,157,204,140],
+            pickable=True)
+        r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1], initial_view_state=view_state)
+        
+            
+    elif len(options) >= 2:
+        layer1 = pdk.Layer(
+            'ScatterplotLayer',
+            df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
+            get_position=['lng', 'lt'],
+            auto_highlight=True,
+            get_radius=50,
+            get_fill_color=[132,157,204,140],
+            pickable=True)
+    
+        layer2 = pdk.Layer(
+            'ScatterplotLayer',
+            df.loc[df['ns1:Name'] == options[1]][['lng','lt']],
+            get_position=['lng', 'lt'],
+            auto_highlight=True,
+            get_radius=50,
+            get_fill_color=[290,187,105,140],
+            pickable=True)
+        r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1,layer2], initial_view_state=view_state)
+    
+    else:
+        r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", initial_view_state=view_state)  
      
-    layer1 = pdk.Layer(
-        'ScatterplotLayer',
-        df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
-        get_position=['lng', 'lt'],
-        auto_highlight=True,
-        get_radius=50,
-        get_fill_color=[132,157,204,140],
-        pickable=True)
+#     layer1 = pdk.Layer(
+#         'ScatterplotLayer',
+#         df.loc[df['ns1:Name'] == options[0]][['lng','lt']],
+#         get_position=['lng', 'lt'],
+#         auto_highlight=True,
+#         get_radius=50,
+#         get_fill_color=[132,157,204,140],
+#         pickable=True)
     
-    layer2 = pdk.Layer(
-        'ScatterplotLayer',
-        df.loc[df['ns1:Name'] == options[1]][['lng','lt']],
-        get_position=['lng', 'lt'],
-        auto_highlight=True,
-        get_radius=50,
-        get_fill_color=[290,187,105,140],
-        pickable=True)
+#     layer2 = pdk.Layer(
+#         'ScatterplotLayer',
+#         df.loc[df['ns1:Name'] == options[1]][['lng','lt']],
+#         get_position=['lng', 'lt'],
+#         auto_highlight=True,
+#         get_radius=50,
+#         get_fill_color=[290,187,105,140],
+#         pickable=True)
     
-    r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1,layer2], initial_view_state=view_state)
+#     r = pdk.Deck(map_style="mapbox://styles/mapbox/satellite-v9", layers=[layer1,layer2], initial_view_state=view_state)
     st.write(r)
 
 
